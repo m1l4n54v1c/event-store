11e9508d793909654bfd9cca4167fd16404f77f9
Milan Savic <saviccc.milllan@gmail.com>
2024-08-27 15:31:47 +0200

    Conference - solutions.

--- sample-faculty/src/main/java/io/event/thinking/sample/faculty/model/ChangeCourseCapacityCommandModel.java
+++ sample-faculty/src/main/java/io/event/thinking/sample/faculty/model/ChangeCourseCapacityCommandModel.java
@@ -4,25 +4,86 @@ import io.event.thinking.eventstore.api.Criteria;
 import io.event.thinking.micro.es.CommandModel;
 import io.event.thinking.micro.es.Event;
 import io.event.thinking.sample.faculty.api.command.ChangeCourseCapacity;
+import io.event.thinking.sample.faculty.api.event.CourseCapacityChanged;
+import io.event.thinking.sample.faculty.api.event.CourseCreated;
+import io.event.thinking.sample.faculty.api.event.StudentSubscribed;
+import io.event.thinking.sample.faculty.api.event.StudentUnsubscribed;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+import static io.event.thinking.eventstore.api.Criterion.criterion;
+import static io.event.thinking.eventstore.api.Tag.tag;
+import static io.event.thinking.micro.es.Event.event;
+import static io.event.thinking.micro.es.Tags.type;
+import static io.event.thinking.sample.faculty.model.Constants.COURSE_ID;
+import static io.event.thinking.sample.faculty.model.Constants.STUDENT_ID;
 
 public class ChangeCourseCapacityCommandModel implements CommandModel<ChangeCourseCapacity> {
 
-    @Override
-    public Criteria criteria(ChangeCourseCapacity command) {
-        // TODO implement
-        return null;
-    }
+    private String courseId;
+    private final List<String> subscribedStudents = new LinkedList<>();
 
     @Override
-    public void onEvent(Event event) {
-        // TODO implement
+    public Criteria criteria(ChangeCourseCapacity cmd) {
+        return Criteria.criteria(criterion(type(CourseCreated.NAME), tag(COURSE_ID, cmd.courseId())),
+                                 criterion(type(StudentSubscribed.NAME), tag(COURSE_ID, cmd.courseId())),
+                                 criterion(type(StudentUnsubscribed.NAME), tag(COURSE_ID, cmd.courseId())));
+    }
+
+    void on(CourseCreated evt) {
+        this.courseId = evt.id();
+    }
+
+
+    void on(StudentSubscribed evt) {
+        subscribedStudents.add(evt.studentId());
+    }
+
+    void on(StudentUnsubscribed evt) {
+        subscribedStudents.remove(evt.studentId());
     }
 
     @Override
     public List<Event> handle(ChangeCourseCapacity command) {
-        // TODO implement
-        return List.of();
+        if (command.newCapacity() < 0) {
+            throw new RuntimeException("Course with given id does not exist");
+        }
+
+        if (courseId == null) {
+            throw new RuntimeException("Course with given id does not exist");
+        }
+
+        List<Event> events = new ArrayList<>();
+        events.add(tagEvent(new CourseCapacityChanged(command.courseId(), command.newCapacity())));
+        if (command.newCapacity() < subscribedStudents.size()) {
+            subscribedStudents.subList(command.newCapacity(), subscribedStudents.size())
+                              .forEach(studentId -> events.add(tagEvent(new StudentUnsubscribed(studentId, courseId))));
+        }
+        return events;
+    }
+
+    private static Event tagEvent(CourseCapacityChanged event) {
+        return event(event,
+                     type(CourseCapacityChanged.NAME),
+                     tag(COURSE_ID, event.id()));
+    }
+
+    private static Event tagEvent(StudentUnsubscribed event) {
+        return event(event,
+                     type(StudentUnsubscribed.NAME),
+                     tag(COURSE_ID, event.courseId()),
+                     tag(STUDENT_ID, event.studentId()));
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        switch (event.payload()) {
+            case CourseCreated e -> on(e);
+            case StudentSubscribed e -> on(e);
+            case StudentUnsubscribed e -> on(e);
+            default -> throw new RuntimeException("No handler for this event");
+        }
     }
 }
--- sample-faculty/src/main/java/io/event/thinking/sample/faculty/model/RenameCourseCommandModel.java
+++ sample-faculty/src/main/java/io/event/thinking/sample/faculty/model/RenameCourseCommandModel.java
@@ -4,25 +4,50 @@ import io.event.thinking.eventstore.api.Criteria;
 import io.event.thinking.micro.es.CommandModel;
 import io.event.thinking.micro.es.Event;
 import io.event.thinking.sample.faculty.api.command.RenameCourse;
+import io.event.thinking.sample.faculty.api.event.CourseCreated;
+import io.event.thinking.sample.faculty.api.event.CourseRenamed;
 
 import java.util.List;
 
+import static io.event.thinking.eventstore.api.Criterion.criterion;
+import static io.event.thinking.eventstore.api.Tag.tag;
+import static io.event.thinking.micro.es.Event.event;
+import static io.event.thinking.micro.es.Tags.type;
+import static io.event.thinking.sample.faculty.model.Constants.COURSE_ID;
+
 public class RenameCourseCommandModel implements CommandModel<RenameCourse> {
+
+    private String courseId;
 
     @Override
     public Criteria criteria(RenameCourse command) {
-        //TODO implement
-        return null;
+        return Criteria.criteria(criterion(type(CourseCreated.NAME),
+                                           tag(COURSE_ID, command.courseId())));
+    }
+
+    private void on(CourseCreated evt) {
+        courseId = evt.id();
     }
 
     @Override
     public void onEvent(Event event) {
-        //TODO implement
+        switch (event.payload()) {
+            case CourseCreated e -> on(e);
+            default -> throw new RuntimeException("No handler for this event");
+        }
     }
 
     @Override
     public List<Event> handle(RenameCourse command) {
-        //TODO implement
-        return List.of();
+        if (courseId == null) {
+            throw new RuntimeException("Course with given id does not exist");
+        }
+        return List.of(tagEvent(new CourseRenamed(courseId, command.newName())));
+    }
+
+    private static Event tagEvent(CourseRenamed event) {
+        return event(event,
+                     type(CourseRenamed.NAME),
+                     tag(COURSE_ID, event.courseId()));
     }
 }
