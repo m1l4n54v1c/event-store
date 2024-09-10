e5055c3218b084ccad21e957091e4b1dbc7eb948
Milan Savic <saviccc.milllan@gmail.com>
2024-09-10 18:19:25 +0200

    Adapted models to handlers.

--- sample-faculty/src/main/java/io/event/thinking/sample/faculty/commandhandler/ChangeCourseCapacityCommandHandler.java
+++ sample-faculty/src/main/java/io/event/thinking/sample/faculty/commandhandler/ChangeCourseCapacityCommandHandler.java
@@ -4,32 +4,100 @@ import io.event.thinking.eventstore.api.Criteria;
 import io.event.thinking.micro.es.DcbCommandHandler;
 import io.event.thinking.micro.es.Event;
 import io.event.thinking.sample.faculty.api.command.ChangeCourseCapacity;
+import io.event.thinking.sample.faculty.api.event.CourseCapacityChanged;
+import io.event.thinking.sample.faculty.api.event.CourseCreated;
+import io.event.thinking.sample.faculty.api.event.StudentSubscribed;
+import io.event.thinking.sample.faculty.api.event.StudentUnsubscribed;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class ChangeCourseCapacityCommandHandler implements DcbCommandHandler<ChangeCourseCapacity, ChangeCourseCapacityCommandHandler.State> {
+import static io.event.thinking.eventstore.api.Criterion.criterion;
+import static io.event.thinking.micro.es.Event.event;
+import static io.event.thinking.micro.es.Indices.typeIndex;
+import static io.event.thinking.sample.faculty.commandhandler.Indices.courseIdIndex;
+import static io.event.thinking.sample.faculty.commandhandler.Indices.studentIdIndex;
+import static java.util.Collections.emptyList;
+
+public class ChangeCourseCapacityCommandHandler
+        implements DcbCommandHandler<ChangeCourseCapacity, ChangeCourseCapacityCommandHandler.State> {
 
     @Override
     public Criteria criteria(ChangeCourseCapacity command) {
-        return null;
+        return Criteria.criteria(criterion(typeIndex(CourseCreated.NAME), courseIdIndex(command.courseId())),
+                                 criterion(typeIndex(StudentSubscribed.NAME), courseIdIndex(command.courseId())),
+                                 criterion(typeIndex(StudentUnsubscribed.NAME), courseIdIndex(command.courseId())));
     }
 
     @Override
     public State initialState() {
-        return null;
+        return State.initial();
     }
 
     @Override
     public State source(Object event, State state) {
-        return null;
+        return switch (event) {
+            case CourseCreated e -> state.withCourseId(e.id());
+            case StudentSubscribed e -> state.withStudentSubscribed(e.studentId());
+            case StudentUnsubscribed e -> state.withStudentUnsubscribed(e.studentId());
+            default -> throw new RuntimeException("No handler for this event");
+        };
     }
 
     @Override
     public List<Event> handle(ChangeCourseCapacity command, State state) {
-        return List.of();
+        if (command.newCapacity() < 0) {
+            throw new RuntimeException("Course capacity cannot be negative");
+        }
+
+        state.assertCourseExists();
+
+        List<Event> events = new ArrayList<>();
+        var courseCapacityChanged = new CourseCapacityChanged(command.courseId(), command.newCapacity());
+        events.add(event(courseCapacityChanged,
+                         typeIndex(CourseCapacityChanged.NAME),
+                         courseIdIndex(courseCapacityChanged.id())));
+        var subscribedStudents = state.subscribedStudents();
+        if (command.newCapacity() < subscribedStudents.size()) {
+            subscribedStudents.subList(command.newCapacity(), subscribedStudents.size())
+                              .forEach(studentId -> {
+                                  var studentSubscribed = new StudentUnsubscribed(studentId, state.courseId());
+                                  events.add(event(studentSubscribed,
+                                                   typeIndex(StudentUnsubscribed.NAME),
+                                                   courseIdIndex(studentSubscribed.courseId()),
+                                                   studentIdIndex(studentSubscribed.studentId())));
+                              });
+        }
+        return events;
     }
 
-    public record State() {
+    public record State(String courseId, List<String> subscribedStudents) {
 
+        public static State initial() {
+            return new State(null, emptyList());
+        }
+
+        public State withCourseId(String courseId) {
+            return new State(courseId, subscribedStudents);
+        }
+
+        public State withStudentSubscribed(String studentId) {
+            List<String> ids = new LinkedList<>(subscribedStudents);
+            ids.add(studentId);
+            return new State(courseId, ids);
+        }
+
+        public State withStudentUnsubscribed(String studentId) {
+            List<String> ids = new LinkedList<>(subscribedStudents);
+            ids.remove(studentId);
+            return new State(courseId, ids);
+        }
+
+        public void assertCourseExists() {
+            if (courseId == null) {
+                throw new RuntimeException("Course with given id does not exist");
+            }
+        }
     }
 }
--- sample-faculty/src/main/java/io/event/thinking/sample/faculty/commandhandler/RenameCourseCommandHandler.java
+++ sample-faculty/src/main/java/io/event/thinking/sample/faculty/commandhandler/RenameCourseCommandHandler.java
@@ -4,32 +4,69 @@ import io.event.thinking.eventstore.api.Criteria;
 import io.event.thinking.micro.es.DcbCommandHandler;
 import io.event.thinking.micro.es.Event;
 import io.event.thinking.sample.faculty.api.command.RenameCourse;
+import io.event.thinking.sample.faculty.api.event.CourseCreated;
+import io.event.thinking.sample.faculty.api.event.CourseRenamed;
 
 import java.util.List;
+
+import static io.event.thinking.eventstore.api.Criterion.criterion;
+import static io.event.thinking.micro.es.Event.event;
+import static io.event.thinking.micro.es.Indices.typeIndex;
+import static io.event.thinking.sample.faculty.commandhandler.Indices.courseIdIndex;
 
 public class RenameCourseCommandHandler implements DcbCommandHandler<RenameCourse, RenameCourseCommandHandler.State> {
 
     @Override
     public Criteria criteria(RenameCourse command) {
-        return null;
+        return Criteria.criteria(criterion(typeIndex(CourseCreated.NAME), courseIdIndex(command.courseId())),
+                                 criterion(typeIndex(CourseRenamed.NAME), courseIdIndex(command.courseId())));
     }
 
     @Override
     public State initialState() {
-        return null;
+        return State.initial();
     }
 
     @Override
     public State source(Object event, State state) {
-        return null;
+        return switch (event) {
+            case CourseCreated e -> state.withCourseCreated(e);
+            case CourseRenamed e -> state.withCourseName(e.newName());
+            default -> throw new RuntimeException("No handler for this event");
+        };
     }
 
     @Override
     public List<Event> handle(RenameCourse command, State state) {
-        return List.of();
+        if (command.newName().equals(state.courseName())) {
+            throw new RuntimeException("Course already has this name");
+        }
+
+        state.assertCourseExists();
+
+        return List.of(event(new CourseRenamed(command.courseId(), command.newName()),
+                             typeIndex(CourseRenamed.NAME),
+                             courseIdIndex(command.courseId())));
     }
 
-    public record State() {
+    public record State(String courseId, String courseName) {
 
+        public static State initial() {
+            return new State(null, null);
+        }
+
+        public State withCourseCreated(CourseCreated evt) {
+            return new State(evt.id(), evt.name());
+        }
+
+        public State withCourseName(String courseName) {
+            return new State(courseId, courseName);
+        }
+
+        public void assertCourseExists() {
+            if (courseId == null) {
+                throw new RuntimeException("Course with given id does not exist");
+            }
+        }
     }
 }
